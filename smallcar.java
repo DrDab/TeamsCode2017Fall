99b9bb95a12811d58e9d02f8f73539a5fb7eb79e
_c0da_ <duvictor514@gmail.com>
2017-10-22 09:51:43 -0700

    Create smallcar.java

---
+++ smallcar.java
@@ -0,0 +1,10 @@
+public class smallcar
+{
+	public static void main (String[] args)
+	{
+		System.out.println("    .-----.");
+		System.out.println(" __//.{)|_\\\\._");
+		System.out.println("/___         ___:");
+		System.out.println("--(_)------(_)--");
+	}
+}
